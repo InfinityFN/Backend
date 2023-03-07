0013dfda0e8ebd095a8374068900cdd03d018f5e
zinx <63958372+zinx-YT@users.noreply.github.com>
2023-03-07 20:15:25 +0000

    fixed crash

--- src/routes/store.js
+++ src/routes/store.js
@@ -1,4 +1,5 @@
-const axios = require("axios")
+const fs = require('fs');
+const path = require('path');
 class Store {
     constructor() {
         this.application = require("express").Router()
@@ -6,46 +7,88 @@ class Store {
     }
 
     endpoints(application) {
-        application.all("/fortnite/api/storefront/v2/gift/check_eligibility/recipient/:friendID/offer/:offerID", async (req,res) => {
-            var shop = (await axios.get("http://localhost:6969/fortnite/api/storefront/v2/catalog")).data;
-            var OfferID = decodeURI(req.params.offerID)
-        
-            var CatalogPurchaseID = null
-        
-            for (storefront of shop.storefronts) {
-                for (storefrontSIR of storefront.catalogEntries) {
-                    if(storefrontSIR.offerId == OfferID){
-                        CatalogPurchaseID = storefrontSIR;
-                    }
-                }
-            }
-            //console.log(CatalogPurchaseID)
-        
-        
-        
-            res.json({
-                "price": {
-                  "currencyType": "MtxCurrency",
-                  "currencySubType": "",
-                  "regularPrice": CatalogPurchaseID["regularPrice"],
-                  "dynamicRegularPrice": CatalogPurchaseID["dynamicRegularPrice"],
-                  "finalPrice": CatalogPurchaseID["finalPrice"],
-                  "saleExpiration": "9999-12-31T23:59:59.999Z",
-                  "basePrice": CatalogPurchaseID["basePrice"]
-                },
-                "items": CatalogPurchaseID["itemGrants"]
-              })
-        })
+        application.get("/fortnite/api/storefront/v2/catalog", async (req, res) => {
 
-        application.all("/fortnite/api/storefront/v2/catalog", async (req, res) => {
-            const catalog = require('../services/resources/json/store.json');
-            res.json(catalog);
+            const test = require('../services/resources/json/store.json');
+            res.json(this.getItemShop(fs.readFileSync(path.join(__dirname, '../services/resources/json/store.json')), fs.readFileSync(path.join(__dirname, '../services/resources/json/catalog_config.json'))));
         });
-        
+
         // lazy <3
         application.get("/catalog/api/shared/bulk/offers", async (req, res) => {
             res.json({});
         });
+    }
+
+    getItemShop(catalog, catalogConfig) {
+        const dailyStorefront = catalog.storefronts.find(storefront => storefront.name === "BRDailyStorefront");
+        const weeklyStorefront = catalog.storefronts.find(storefront => storefront.name === "BRWeeklyStorefront");
+
+        for (const [key, value] of Object.entries(catalogConfig)) {
+            if (Array.isArray(value.itemGrants) && value.itemGrants.length > 0) {
+                const catalogEntry = {
+                    devName: "",
+                    offerId: "",
+                    fulfillmentIds: [],
+                    dailyLimit: -1,
+                    weeklyLimit: -1,
+                    monthlyLimit: -1,
+                    categories: [],
+                    prices: [
+                        {
+                            currencyType: "MtxCurrency",
+                            currencySubType: "",
+                            regularPrice: 0,
+                            finalPrice: 0,
+                            saleExpiration: "9999-12-02T01:12:00Z",
+                            basePrice: 0,
+                        },
+                    ],
+                    matchFilter: "",
+                    filterWeight: 0,
+                    appStoreId: [],
+                    requirements: [],
+                    offerType: "StaticPrice",
+                    giftInfo: {
+                        bIsEnabled: false,
+                        forcedGiftBoxTemplateId: "",
+                        purchaseRequirements: [],
+                        giftRecordIds: [],
+                    },
+                    refundable: true,
+                    metaInfo: [],
+                    displayAssetPath: "",
+                    itemGrants: [],
+                    sortPriority: 0,
+                    catalogGroupPriority: 0,
+                };
+
+                for (const itemGrant of value.itemGrants) {
+                    if (typeof itemGrant === "string" && itemGrant.length > 0) {
+                        catalogEntry.devName = itemGrant;
+                        catalogEntry.offerId = itemGrant;
+
+                        catalogEntry.requirements.push({
+                            requirementType: "DenyOnItemOwnership",
+                            requiredId: itemGrant,
+                            minQuantity: 1,
+                        });
+                        catalogEntry.itemGrants.push({ templateId: itemGrant, quantity: 1 });
+                    }
+                }
+
+                catalogEntry.prices[0].basePrice = value.price;
+                catalogEntry.prices[0].regularPrice = value.price;
+                catalogEntry.prices[0].finalPrice = value.price;
+
+                if (key.toLowerCase().startsWith("daily") && dailyStorefront) {
+                    dailyStorefront.catalogEntries.push(catalogEntry);
+                } else if (key.toLowerCase().startsWith("featured") && weeklyStorefront) {
+                    weeklyStorefront.catalogEntries.push(catalogEntry);
+                }
+            }
+        }
+
+        return catalog;
     }
 }
 
