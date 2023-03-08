38290778f771bba7cf42fe3220cdeda021e86fd8
zinx <63958372+zinx-YT@users.noreply.github.com>
2023-03-08 17:12:30 +0000

    revert

---
+++ src/services/modules/Admin.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const data101 = mongoose.Schema({
+   "key": "yasssssss"
+   "playlists": {
+      type: Array,
+      default: []
+   }
+})
+
+module.exports = mongoose.model("admin", data101
