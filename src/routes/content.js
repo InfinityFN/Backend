7ac6d70d020f58e069299d6e388c37bf5e9cc288
zinx <63958372+zinx-YT@users.noreply.github.com>
2023-03-09 13:39:57 +0000

    Update content.js

--- src/routes/content.js
+++ src/routes/content.js
@@ -6,32 +6,77 @@ class FortniteGame {
     }
     endpoints(application){
         application.get(["/content/api/pages/fortnite-game", "/content/app/pages/"], async (req,res) => {
-             const newsalive = await AdminMod.findOne({ _id: new ObjectId("6408cefd0e072e39fd5d7ebf") }).lean().catch(e => next(e))
+             const fortnitecontentyay = await AdminMod.findOne({ _id: new ObjectId("6408cefd0e072e39fd5d7ebf") }).lean().catch(e => next(e))
             res.json({
  "_title": "Fortnite Game",
     "_activeDate": "2017-08-30T03:20:48.050Z",
     "lastModified": "2019-11-01T17:33:35.346Z",
     "_locale": "en-US",
+                   "loginmessage": {
+      "_title": "LoginMessage",
+      "loginmessage": {
+        "_type": "CommonUI Simple Message",
+        "message": {
+          "_type": "CommonUI Simple Message Base",
+          "title": "Infinity",
+          "body": "Welcome to the Infinity Beta\nDiscord: https://discord.gg/gtxWefsyfv"
+        }
+      },
+      "_activeDate": "2017-07-19T13:14:04.490Z",
+      "lastModified": "2018-03-15T07:10:22.222Z",
+      "_locale": "en-US"
+    },
+                    "savetheworldnews": {
+      "news": {
+        "_type": "Battle Royale News",
+        "messages": [
+          {
+            "image": fortnitecontentyay.news.news1.image,
+            "hidden": false,
+            "_type": "CommonUI Simple Message Base",
+            "adspace": fortnitecontentyay.news.news1.adspace,
+            "title": fortnitecontentyay.news.news1.title,
+            "body": fortnitecontentyay.news.news1.body,
+            "spotlight": false
+          },
+          {
+            "image": fortnitecontentyay.news.news2.image,
+            "hidden": false,
+            "_type": "CommonUI Simple Message Base",
+            "adspace": fortnitecontentyay.news.news2.adspace,
+            "title": fortnitecontentyay.news.news2.title,
+            "body": fortnitecontentyay.news.news2.body,
+            "spotlight": false
+          }
+        ]
+      },
+      "_title": "SaveTheWorldNews",
+      "_noIndex": false,
+      "alwaysShow": false,
+      "_activeDate": "2018-08-06T18:25:46.770Z",
+      "lastModified": "2019-10-30T20:17:48.789Z",
+      "_locale": "en-US"
+    },
                     "creativenews": {
       "news": {
         "_type": "Battle Royale News",
         "messages": [
           {
-            "image": "https://i.pinimg.com/originals/d0/a3/c3/d0a3c3ffade9804fad4075b13323d31d.png",
+            "image": fortnitecontentyay.news.news1.image,
             "hidden": false,
             "_type": "CommonUI Simple Message Base",
-            "adspace": "JOIN OUR COMMUNITY!",
-            "title": "Join my discord server!",
-            "body": "https://discord.gg/gtxWefsyfv",
+            "adspace": fortnitecontentyay.news.news1.adspace,
+            "title": fortnitecontentyay.news.news1.title,
+            "body": fortnitecontentyay.news.news1.body,
             "spotlight": false
           },
           {
-            "image": "https://media.discordapp.net/attachments/1080368888342130829/1080370749044432917/image.png?width=1201&height=671",
+            "image": fortnitecontentyay.news.news2.image,
             "hidden": false,
             "_type": "CommonUI Simple Message Base",
-            "adspace": "BETA!",
-            "title": "Infinity",
-            "body": "Welcome to Infinity!",
+            "adspace": fortnitecontentyay.news.news2.adspace,
+            "title": fortnitecontentyay.news.news2.title,
+            "body": fortnitecontentyay.news.news2.body,
             "spotlight": false
           }
         ]
