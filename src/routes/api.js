a783060011e9861a9f909c0fe8bbb18775a4ecab
John Shier <slushnix@gmail.com>
2023-03-09 00:10:07 -0500

    Added Ingame Reporting & Semi Feedback System

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,4 +1,5 @@
 const User = require("../services/modules/User")
+const { WebhookClient, MessageEmbed } = require('discord.js');
 
 
 class Api {
@@ -8,6 +9,107 @@ class Api {
     }
     endpoints(application) {
 
+        application.use((req, res, next) => {
+            if(require('../config.json').logRequests == true) {
+                console.log(`Incoming request: ${req.method} ${req.url}`)
+            }
+            
+            next()
+        })
+
+        // JUST to test to see how the embed looked. will delete later
+        application.post('/report/test', (req, res) => {
+            // report webhook
+            const webhook = new WebhookClient('1083247535575015455', '32LrEwxSdBBevi82lbHuQm0bzluWgtGzs1IVnC-nNf4OBeVecBK9RmP1nTCUrMi-XBnE');
+            const embed = new MessageEmbed()
+                .setTitle('New Report Recieved!')
+                .setColor('#ff1100')
+                .setDescription('A new report has been recieved!')
+                .addFields(
+                    { name: 'Reporter', value: "Kohl" },
+                    { name: 'Offender', value: "zinx" },
+                    { name: 'Category', value: "CommunicationsAbuse" },
+                    { name: 'Playlist', value: "Playlist_DefaultDuo" },
+                    { name: 'Details', value: "he being meanie" }
+                );
+
+            // Send the embed to the webhook
+            webhook.send(embed)
+                .then(() => console.log('Embed sent successfully!'))
+                .catch(console.error);
+
+        });
+
+        // No stealing <3 (Ingame Report)
+        application.post("/fortnite/api/game/v2/toxicity/account/:accountId/report/:offenderAccountId", async (req, res) => {
+            var Reporter = await User.findOne({ id: req.params.accountId }).lean();
+            var Offender = await User.findOne({ id: req.params.offenderAccountId }).lean();
+            console.log(Offender);
+
+            if(Offender.displayName == null || Offender.displayName == "" || Offender.displayName == undefined) {
+                return console.log("Offender isn't found");
+            }
+
+            // report webhook
+            const webhook = new WebhookClient('1083247535575015455', '32LrEwxSdBBevi82lbHuQm0bzluWgtGzs1IVnC-nNf4OBeVecBK9RmP1nTCUrMi-XBnE');
+
+            /*
+            {
+  reason: 'CommunicationsAbuse',
+  details: '',
+  gameSessionId: 'REPLAY-96a1b17c400bdd44f7d04898b43fa43c',
+  creativeIslandSharingLink: '',
+  creativeIslandGuid: '',
+  creativeIslandOwnerAccountId: '',
+  playlistName: 'Playlist_DefaultDuo',
+  bIsCompetitiveEvent: false,
+  reporterPlatform: 'Windows',
+  offenderPlatform: '',
+  bBlockUserRequested: false,
+  bUserMarkedAsKnown: false
+}
+             */
+            console.log(Reporter.displayName);
+
+            // Create a new MessageEmbed object
+            const embed = new MessageEmbed()
+                .setTitle('New Report Recieved!')
+                .setColor('#ff1100')
+                .setDescription('A new report has been recieved!')
+                .addFields(
+                    { name: 'Reporter', value: Reporter.displayName },
+                    { name: 'Offender', value: Offender.displayName },
+                    { name: 'Category', value: req.body.reason },
+                    { name: 'Playlist', value: req.body.playlistName },
+                    { name: 'Details', value: req.body.details }
+                );
+
+            // Send the embed to the webhook
+            webhook.send({ embeds: [embed] })
+                .then(() => console.log('Embed sent successfully!'))
+                .catch(console.error);
+
+
+            console.log('ReportTest');
+
+            res.status(204);
+            res.json({});
+        });
+
+        // Feedback for Comment (not done *lemme do it zinx*)
+        application.all("/fortnite/api/feedback/Comment", (req, res) => {
+            console.log("Comment!");
+            console.log(req);
+            res.status(200);
+            res.json({});
+        });
+
+        // Feedback for Bug (not done *lemme do it zinx*)
+        application.all("/fortnite/api/feedback/Bug", (req, res) => {
+            console.log("BUG!");
+            res.status(200);
+            res.json({});
+        });
 
         application.get("/fortnite/api/v2/versioncheck/*", (req, res) => {
             res.json({
@@ -48,22 +150,22 @@ class Api {
             const accountId = req.query.accountId
 
             var Data = []
-            if(Array.isArray(accountId)) {
-                for(const index of accountId){
+            if (Array.isArray(accountId)) {
+                for (const index of accountId) {
                     var Accounts = await User.findOne({ id: index }).lean();
-                    if(Accounts){
+                    if (Accounts) {
                         Data.push({
                             "id": index,
                             "displayName": Accounts.displayName,
                             "externalAuths": {}
                         })
-                    }else{
+                    } else {
 
                     }
                 }
-            }else{
-                var Accounts = await User.findOne({ id: accountId}).lean();
-                if(Accounts){
+            } else {
+                var Accounts = await User.findOne({ id: accountId }).lean();
+                if (Accounts) {
                     Data.push({
                         "id": Accounts.id,
                         "links": {},
